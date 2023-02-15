1aa5b3ce93339acdb256a9325be6df15b7313758
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-14 18:03:18 -0800

    make the toggle functionality work by managing the state using redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((store) => store.counter);
+  const show = useSelector((store) => store.showCounter);
   const dispatch = useDispatch();
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -15,12 +16,14 @@ const Counter = () => {
     dispatch({ type: "increase", amount: 5 });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase By 5</button>
