d675ee16a43744b0633cc97b3cb09bb720378800
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-14 20:13:31 -0800

    use reduxjs/toolkit for maintaining state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import classes from "./Counter.module.css";
 
+import { counterActions } from "../store/index";
+
 import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
@@ -7,17 +9,17 @@ const Counter = () => {
   const show = useSelector((store) => store.showCounter);
   const dispatch = useDispatch();
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
